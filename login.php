7adc95690a5ba7dd31b5e6549ff3dbee14694169
Daniel Ugalde <daniel@ugalde.cl>
2022-04-22 12:08:37 -0400

    Guardar login

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -39,5 +39,4 @@
 </header>
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
