a8474857dff621f39339f6be83322aff790ee0c7
Daniel Ugalde <daniel@ugalde.cl>
2022-04-22 20:11:27 -0400

    Subir archivos

--- login.php
+++ login.php
@@ -5,28 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-
-    <title>Sistema de Mantenimiento</title>   
-    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <!--favicon-->
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+        <link rel="manifest" href="img/site.webmanifest">
+        <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <!--nav-->
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+    <title>Sistema de Mantenimiento</title> 
     </head>
 <body>
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Carousel</a>
+      <a class="navbar-brand" href="#"><img src="img/icono.png" width="80" title="Maintenance System"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="index.html">home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" aria-current="page" href="contacto.php">Contacto</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link  active" aria-current="page" href="login.php">Ingresar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="registro.php">Registrase</a>
           </li>
         </ul>
         <form class="d-flex">
@@ -37,6 +48,7 @@
     </div>
   </nav>
 </header>
+<!--Permite que se desplegue el Menu-->
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
